9c56df9aedc3d6be6075b431a046be87464cebaf
MadhuHS <madhuhs943@gmail.com>
2016-07-08 17:10:52 +0530

    updated codes of movie ratings app
    08/07/2016

--- MovieRatingsApp/app/src/main/java/com/jspiders/moblie/movieratingsapp/MainActivity.java
+++ MovieRatingsApp/app/src/main/java/com/jspiders/moblie/movieratingsapp/MainActivity.java
@@ -3,31 +3,67 @@ package com.jspiders.moblie.movieratingsapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView jsontv;
+    ListView movieslv;
+    ArrayList<String> names;
+    ArrayAdapter adapter;
+    ImageView ivposter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        jsontv = (TextView) findViewById(R.id.tvjson);
+        movieslv = (ListView) findViewById(R.id.lvmovies);
+        names = new ArrayList<String>();
+        ivposter = (ImageView) findViewById(R.id.ivposter);
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.get("http://www.omdbapi.com/?s=Batman&page=2", new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
 
-                String response = new String(responseBody);
-                Log.d("DEBUG",response);
-                jsontv.setText(response);
+                String jsonresponse = new String(responseBody);
+                Log.d("DEBUG",jsonresponse);
+
+                Gson gson = new Gson();
+
+                Response response1 = gson.fromJson(jsonresponse,Response.class);
+                ArrayList<Response.Search> list = (ArrayList<Response.Search>) response1.getSearch();
+
+
+                for (Response.Search s : list)
+                {
+                    names.add(s.getTitle());
+                    //Log.d("DEBUG", s.getTitle());
+                }
+
+
+                adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,names);
+                movieslv.setAdapter(adapter);
+
+                Picasso.with(MainActivity.this).load(list.get(0).getPoster()).into(ivposter);
+
+
+
+
             }
 
             @Override
---
+++ MovieRatingsApp/app/src/main/java/com/jspiders/moblie/movieratingsapp/Temp1.java
@@ -0,0 +1,7 @@
+package com.jspiders.moblie.movieratingsapp;
+
+/**
+ * Created by user on 08/07/2016.
+ */
+public class Temp1 {
+}
